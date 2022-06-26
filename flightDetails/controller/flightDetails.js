b8a4fe2479c2b9027d5177d7ac2ac29ebf4af0bf
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-06-26 19:54:42 +0100

    add error handing

--- flightDetails/controller/flightDetails.js
+++ flightDetails/controller/flightDetails.js
@@ -1,10 +1,13 @@
 const FlightDetails = require('../model/flightDetails');
 
 const getFlightDetailsByFlightNumber = async (req, res) => {
-    const flightDetails = await FlightDetails.getFlightDetails(req.params.flightNumber);
-
-    // TODO add error handling.
-    res.status(200).send(flightDetails);
+    try {
+        const flightDetails = await FlightDetails.getFlightDetails(req.params.flightNumber);
+        // TODO add error handling.
+        res.status(200).send(flightDetails);
+    } catch (error) {
+        res.status(400).send('error');
+    }
 };
 
 module.exports = { getFlightDetailsByFlightNumber };
