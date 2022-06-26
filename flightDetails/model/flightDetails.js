9cc4e00e2ef18d4f4a5373f758eb7e1d08558e56
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-06-26 11:34:26 +0100

    add flightDetails model

---
+++ flightDetails/model/flightDetails.js
@@ -0,0 +1,20 @@
+class FlightDetails {
+    constructor() {
+     this.status = "en route",
+     this.flightCode = "DC2812",
+     this.departedCity = "London"
+     this.departedName = "Heathrow",
+     this.departedCode = "LHR"
+     this.destinationCity = "New York City",
+     this.destinationName = "John F. Kennedy Airport",
+     this.destinationCode = "JFK"
+     this.flightTime = 1234,
+     this.milesFlown = 123,
+     this.milesRemaining = 123,
+     this.percentComplete = .5,
+     this.departureTime = "05:21",
+     this.arrivalTime = "09:30"
+    }
+
+    static getFlightDetails(flightNumber) {}
+}
